9efdb94421c407ee5dc5200b2071325236f50c7e
theforest <theforest@mail.com>
2021-10-05 11:13:53 +0300

    refactoring package

--- src/main/java/com/theforest/apachecommonsexample/hard/pool/PoolExample.java
+++ src/main/java/com/theforest/apachecommonsexample/hard/pool/PoolExample.java
@@ -1,4 +1,4 @@
-package com.theforest.apachecommonsexample.pool;
+package com.theforest.apachecommonsexample.hard.pool;
 
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.ObjectPool;
--- src/main/java/com/theforest/apachecommonsexample/proxy/ProxyExample.java
+++
@@ -1,7 +0,0 @@
-package com.theforest.apachecommonsexample.proxy;
-
-/**
- * Library for creating dynamic proxies.
- */
-public class ProxyExample {
-}
